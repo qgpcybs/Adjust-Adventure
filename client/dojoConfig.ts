3226000f99aec99de934da2d4335fb59e109312b
7Star <xiaochi@mono.fun>
2024-06-23 21:06:23 +0800

    change the config

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -3,5 +3,8 @@ import { createDojoConfig } from "@dojoengine/core";
 
 export const dojoConfig = createDojoConfig({
     manifest,
-    toriiUrl: "http://127.0.0.1:8080",
+    rpcUrl: 'https://api.cartridge.gg/x/7stars2/katana',
+    toriiUrl: "https://api.cartridge.gg/x/7stars2/torii",
+    masterAddress: '0x5c61800296f9045154983b5fe4edb73ae35fda2af2d9e71638c89ae9d8ebc86',
+    masterPrivateKey: '0x1ed5be6078e05632b91edc1deb7b56b460b75d44463e62ab619a560f9dfb2f8'
 });
